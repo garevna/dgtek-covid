068891d5dc222a3b574a650fd96aab7a88490a52
Irina Fylyppova (via Travis CI) <irina.h.fylyppova@gmail.com>
2020-03-30 00:31:26 +0000

    Deploy garevna/dgtek-covid to github.com/garevna/dgtek-covid.git:gh-pages

---
+++ precache-manifest.5c5c61508709ecb0b3508ccbfdb22362.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "936a7058ebd495c33a3a",
+    "url": "/dgtek-covid/css/app.42e99e59.css"
+  },
+  {
+    "revision": "9b156c5b2f2676dc9519",
+    "url": "/dgtek-covid/css/chunk-vendors.57d7e4e8.css"
+  },
+  {
+    "revision": "32750b00d6f5003097a67377a4af208f",
+    "url": "/dgtek-covid/fonts/Gilroy-Bold.32750b00.woff"
+  },
+  {
+    "revision": "3cf0ee273a0b3f022234b6572c3b78f9",
+    "url": "/dgtek-covid/fonts/Gilroy-Bold.3cf0ee27.ttf"
+  },
+  {
+    "revision": "5e111ca086414e1d8905e5a875fb6899",
+    "url": "/dgtek-covid/fonts/Gilroy-Bold.5e111ca0.eot"
+  },
+  {
+    "revision": "335c7ebfe69e97fec5ae32fea4fd17fa",
+    "url": "/dgtek-covid/fonts/Gilroy-Heavy.335c7ebf.eot"
+  },
+  {
+    "revision": "8d36efeb3349af073647c0c37ec995f1",
+    "url": "/dgtek-covid/fonts/Gilroy-Heavy.8d36efeb.ttf"
+  },
+  {
+    "revision": "ffa5800ec1a44bef80af3dfdced37e7a",
+    "url": "/dgtek-covid/fonts/Gilroy-Heavy.ffa5800e.woff"
+  },
+  {
+    "revision": "6444f14adcdee041b62184f13139a56d",
+    "url": "/dgtek-covid/fonts/Gilroy-Medium.6444f14a.ttf"
+  },
+  {
+    "revision": "7d5dc1791cf2738d55a87e871549004c",
+    "url": "/dgtek-covid/fonts/Gilroy-Medium.7d5dc179.woff"
+  },
+  {
+    "revision": "ce12e7f23ff25c6f534364a7435885df",
+    "url": "/dgtek-covid/fonts/Gilroy-Medium.ce12e7f2.eot"
+  },
+  {
+    "revision": "59525ce19c9e4999bc19d96e4a88c1b3",
+    "url": "/dgtek-covid/fonts/Gilroy-Regular.59525ce1.eot"
+  },
+  {
+    "revision": "8e8705d069fbb9099b3a1e1d779fa5c5",
+    "url": "/dgtek-covid/fonts/Gilroy-Regular.8e8705d0.woff"
+  },
+  {
+    "revision": "ae5e7255973ffe09b53f07a2805232a8",
+    "url": "/dgtek-covid/fonts/Gilroy-Regular.ae5e7255.ttf"
+  },
+  {
+    "revision": "a2e7c30aefc18a4ac6fa59d7f00dfd24",
+    "url": "/dgtek-covid/img/header-back-large.a2e7c30a.png"
+  },
+  {
+    "revision": "d1ac81431a3f8b54af8882de271bc964",
+    "url": "/dgtek-covid/img/header-back-small.d1ac8143.png"
+  },
+  {
+    "revision": "97274c2b1d0cf0e9db7c5d0e1ae411ea",
+    "url": "/dgtek-covid/img/header-back.97274c2b.png"
+  },
+  {
+    "revision": "2696b6cc854851c416cfb25e7b851218",
+    "url": "/dgtek-covid/img/ico-1.2696b6cc.svg"
+  },
+  {
+    "revision": "5dd010499c65f9038506b3579187c44a",
+    "url": "/dgtek-covid/img/ico-2.5dd01049.svg"
+  },
+  {
+    "revision": "1f39d35850b02ca545010473ace2cb63",
+    "url": "/dgtek-covid/img/ico-3.1f39d358.svg"
+  },
+  {
+    "revision": "54759fbf60e33612e5aafeb3a165b3a8",
+    "url": "/dgtek-covid/img/map-picture.54759fbf.svg"
+  },
+  {
+    "revision": "b6df979e9f32d68441a44efd29e0e417",
+    "url": "/dgtek-covid/img/red-background.b6df979e.png"
+  },
+  {
+    "revision": "b03ecf284653f1d5864f06dea82de860",
+    "url": "/dgtek-covid/img/top-picture.b03ecf28.png"
+  },
+  {
+    "revision": "bad3275a2b2cd87321bb1efff7b31b9b",
+    "url": "/dgtek-covid/index.html"
+  },
+  {
+    "revision": "936a7058ebd495c33a3a",
+    "url": "/dgtek-covid/js/app.cd1a59b4.js"
+  },
+  {
+    "revision": "9b156c5b2f2676dc9519",
+    "url": "/dgtek-covid/js/chunk-vendors.bc96ffab.js"
+  },
+  {
+    "revision": "54b85a56675f2244c0dc88048122900c",
+    "url": "/dgtek-covid/manifest.json"
+  }
+]);
